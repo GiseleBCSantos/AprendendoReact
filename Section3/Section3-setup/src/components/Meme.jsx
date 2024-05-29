1cea4c45e0d3835de72e443fc9fce6d77704235f
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-05-29 07:35:23 -0300

    :sparkles: feat: Uso de form e ajustes do texto na imagem

--- Section3/Section3-setup/src/components/Meme.jsx
+++ Section3/Section3-setup/src/components/Meme.jsx
@@ -1,32 +1,76 @@
-import React from 'react'
-import memesData from '../memesData'
+import React, { useId, useState } from 'react'
+// import memesData from '../memesData'
+import { useEffect } from 'react';
 
 const Meme = () => {
     let url;
-    
-    function getMemeImage(){
-        // const randomMeme = memesData.data.memes[Math.floor(Math.random() * memesData.data.memes.length)].url
-        // console.log(randomMeme)
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
 
-        const memesArray = memesData.data.memes
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        url = memesArray[randomNumber].url
-        console.log(url)
+    const [allMemeImages, setAllMemeImages] = useState([])
 
+    const id = useId()
+
+
+    function getMemeImage() {
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        url = allMemeImages[randomNumber].url
+
+        setMeme(prevState => ({
+            ...prevState,
+            randomImage: url
+        }))
     }
+
+    function handleChange(event){
+        const {name, value} = event.target
+
+        setMeme(prevData => ({
+            ...prevData,
+            [name]: value
+        }))
+    }
+
+    useEffect(() => {
+        async function getMemes(){
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemeImages(data.data.memes)
+        }
+        
+        getMemes()
+
+    }, [])
 
     return (
         <main>
             <div className='form'>
                 <div className="form--items">
                     <div className="form--topText">
-                        <label htmlFor="topText">Top text</label>
-                        <input type="text" id='topText' placeholder='Shut up' />
+                        <label htmlFor={id + 'topText'}>Top text</label>
+                        <input
+                            type="text"
+                            id={id + 'topText'}
+                            placeholder='Shut up'
+                            value={meme.topText}
+                            name='topText'
+                            onChange={handleChange}
+                        />
                     </div>
 
                     <div className="form--bottomText">
-                        <label htmlFor="bottomText">Bottom text</label>
-                        <input type="text" id='bottomText' placeholder='And take my money' />
+                        <label htmlFor={id + 'bottomText'}>Bottom text</label>
+                        <input
+                            type="text"
+                            id={id + 'bottomText'}
+                            placeholder='And take my money'
+                            value={meme.bottomText}
+                            name='bottomText'
+                            onChange={handleChange}
+                        />
                     </div>
                 </div>
 
@@ -35,8 +79,10 @@ const Meme = () => {
                 </button>
             </div>
 
-            <div className="image">
-                <img src={`${url}`} alt="" />
+            <div className="meme">
+                <img src={`${meme.randomImage}`} alt="" className='meme--image' />
+                <h2 className='meme--text top'>{meme.topText}</h2>
+                <h2 className='meme--text bottom'>{meme.bottomText}</h2>
             </div>
         </main>
     )
