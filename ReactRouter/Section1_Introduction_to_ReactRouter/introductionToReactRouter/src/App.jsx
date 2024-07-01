51ab6ea2bb8c4b3f8a005ccf577b7f8c73fe2906
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-07-01 11:18:13 -0300

    :sparkles: feat: Add pages

--- ReactRouter/Section1_Introduction_to_ReactRouter/introductionToReactRouter/src/App.jsx
+++ ReactRouter/Section1_Introduction_to_ReactRouter/introductionToReactRouter/src/App.jsx
@@ -2,19 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Home from './components/Home'
-import About from './components/About'
+import Home from './pages/Home'
+import About from './pages/About'
+import Vans from './pages/Vans'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Button from './components/Button'
+import Navbar from './components/navbar/Navbar'
+import Footer from './components/footer/Footer'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/about'}>About</Link>
-        </nav>
+        <Navbar />
         <Routes>
           <Route
             path='/'
@@ -24,8 +25,12 @@ function App() {
             path='/about'
             element={<About />}
           />
+          <Route
+            path='/vans'
+            element={<Vans />}
+          />
         </Routes>
-
+        <Footer />
       </BrowserRouter>
     </>
   )
