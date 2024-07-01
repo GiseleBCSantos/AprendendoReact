723487b1a2df0858dc3ab01dbf7c97c1eb2fee53
GiseleBCSantos <catce.2023111mtds0035@aluno.ifpi.edu.br>
2024-07-01 11:19:17 -0300

    :recycle: refactor: Reorganize

--- ReactRouter/Section1_Introduction_to_ReactRouter/introductionToReactRouter/src/main.jsx
+++ ReactRouter/Section1_Introduction_to_ReactRouter/introductionToReactRouter/src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import About from './components/About.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
